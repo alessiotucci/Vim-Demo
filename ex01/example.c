ba97c7501346f3c0005aa4f5fbd0c788d3aeadf0
atucci <alessiotucci33@gmail.com>
2024-10-08 15:00:36 +0200

    maybe I am ready

---
+++ ex01/example.c
@@ -0,0 +1,12 @@
+int	ft_strlen(char *str)
+{
+
+}
+
+// Help me, I forgot how to code ...
+//
+int	main()
+{
+	ft_strlen("ciao");
+	ft_strchr("ciao", 'y');
+}
---
+++ ex02/example.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   example.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: atucci <atucci@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/10/08 14:30:28 by atucci            #+#    #+#             */
+/*   Updated: 2024/10/08 14:31:19 by atucci           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include <stdio.h>
+
+int main(int argc, char *argv[]) {
+    printf("Number of arguments: %d\n", argc);
+    for (int i = 0; i < argc; i++) {
+        printf("Argument %d: %s\n", i, argv[i]);
+    }
+    return 0;
+}
+
